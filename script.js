511f0445e649a402e5055cb269279def749e308f
Rajesh Jangid <149564962+jeshjangid@users.noreply.github.com>
2024-03-29 13:34:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+let inputSlider = document.getElementById('inputSlider');
+let sliderValue = document.getElementById('sliderValue');
+let passBox = document.getElementById('passBox');
+let lowercase = document.getElementById('lowercase');
+let uppercase = document.getElementById('uppercase');
+let symbols = document.getElementById('symbols');
+let numbers = document.getElementById('numbers');
+let genBtn = document.getElementById('genBtn');
+let copyIcon = document.getElementById('copyIcon');
+
+
+sliderValue.textContent = inputSlider.value;
+
+inputSlider.addEventListener('input', () => {
+    sliderValue.textContent = inputSlider.value;
+
+})
+
+genBtn.addEventListener('click', () => {
+    passBox.value = generatePassword()
+})
+
+let lowerChars = 'abcdefghijklmnopqrstuvwxyz';
+let upperChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+let allNumbers = '0123456789';
+let allSymbols = '!@#$%^&*';
+
+
+// function generatePassword
+function generatePassword(){
+    let genPassword = '';
+    let allChars = '';
+
+    allChars += lowercase.checked ? lowerChars : '';
+    allChars += uppercase.checked ? upperChars : '';
+    allChars += numbers.checked ? allNumbers : '';
+    allChars += symbols.checked ? allSymbols : '';
+
+    if (allChars == '' || allChars.length == 0) {
+        return genPassword
+    } 
+
+    let i = 1;
+    while(i<=inputSlider.value){
+        genPassword += allChars.charAt(Math.floor(Math.random() * allChars.length));
+        i++;
+    }
+    
+    return genPassword;
+}
+
+copyIcon.addEventListener('click', () => {
+    if (passBox.value != '' || passBox.value.length >= 1) {
+        navigator.clipboard.readText(passBox.value);
+        copyIcon.title = 'Copied Password';
+        copyIcon.innerText = 'check';
+        
+        setTimeout(() => {
+            copyIcon.innerText = 'content_copy';
+            copyIcon.title = '';
+            
+        }, 2000);
+    }
+})
